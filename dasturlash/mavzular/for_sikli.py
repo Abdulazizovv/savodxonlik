6c6b18293c1137b58a8785faf3dfe0d3b09aad49
abdulazizovv <aqllibola567@gmail.com>
2025-12-01 16:51:48 +0500

    for sikli yanglar uchun

---
+++ dasturlash/mavzular/for_sikli.py
@@ -0,0 +1,37 @@
+
+
+# ekranga 10 marta salom deb chiqaruvchi
+
+
+#print("Dastur ishga tushdi")
+
+#for i in range(0, 10, 2):
+#    print(i)
+
+#print("Dastur tugadi")
+
+
+
+#for i in range(101):
+#    if i % 3 == 0 and i % 5 == 0:
+#        print(i, " soni 15 bo'linadi")
+#    elif i % 5 == 0:
+#        print(i, " soni 5 bo'linadi")
+#    elif i % 3 == 0:
+#        print(i, " soni 3 bo'linadi")
+    
+
+#n = int(input("Son kiriting:>"))
+
+#jami = 1
+
+#for i in range(1,  n + 1):
+#    jami = jami * i
+
+#print(jami)
+
+
+for i in range(10):
+    if i == 5:
+        break
+    print(i)
