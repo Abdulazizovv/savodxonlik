96540cecd283cd429e596ff1ae9d12d727628e96
abdulazizovv <aqllibola567@gmail.com>
2025-12-03 09:01:39 +0500

    takrorlash uchun topshiriqlar yuklandi

---
+++ dasturlash/topshiriqlar/takrorlash.py
@@ -0,0 +1,56 @@
+
+
+
+# Takrorlash uchun topshiriqlar
+# funksiya yaratib bajarishingiz kerak
+
+# 1-topshiriq
+# ro'yxatdagi unli harf bilan boshlanadigan so'zlarni yangi ro'yxatga joylashtiring
+# sozlar = ['olma', 'anor', 'banan', 'shaftoli', 'uzum', 'nok', 'gilos']
+# natija = ['olma', 'anor', 'uzum']
+
+
+# 2-topshiriq
+# berilgan ro'yxatdagi juft sonlarni yangi ro'yxatga joylashtiring
+# sonlar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# natija = [2, 4, 6, 8, 10]
+
+# 3-topshiriq
+# berilgan matndagi so'zlarni sonini hisoblang. Bunda bir xil so'zlar soni qo'shib hisoblanadi
+# matn = "salom dunyo salom odamlar dunyo"
+# salom: 2, dunyo: 2, odamlar: 1
+
+
+# 4-topshiriq
+# ko'paytirish jadvalini hosil qiling (1 dan 10 gacha)
+# natija:
+# 1 x 1 = 1
+# 1 x 2 = 2
+# 1 x 3 = 3
+# 1 x 4 = 4
+# 1 x 5 = 5
+# 1 x 6 = 6
+# 1 x 7 = 7
+# 1 x 8 = 8
+# 1 x 9 = 9
+# 1 x 10 = 10
+# 2 x 1 = 2
+# 2 x 2 = 4
+# ...
+# 10 x 10 = 100
+
+# 5-topshiriq
+# ro'yxat ichidagi eng katta sonni toping(max funksiyasidan foydalanmang)
+# sonlar = [34, 12, 45, 67, 23, 89, 2, 90, 11]
+# natija = 90
+
+
+# 6-topshiriq
+# berilgan matn ichida raqamlarni ajratib beruvchi funksiya yarating
+# matn = "a1b2c3d4e5"
+# natija = ['1', '2', '3', '4', '5']
+
+# 7-topshiriq | hard
+# sonni stringga aylantirish(str funksiyasidan foydalanmang)
+# son = 12345
+# natija = "12345"
