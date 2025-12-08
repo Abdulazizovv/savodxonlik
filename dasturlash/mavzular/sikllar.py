1aea725c117a086c8761a82fefe9787a3bbce95b
abdulazizovv <aqllibola567@gmail.com>
2025-12-08 16:46:40 +0500

    while sikli topshiriqlar

--- dasturlash/mavzular/sikllar.py
+++ dasturlash/mavzular/sikllar.py
@@ -5,7 +5,7 @@
 
 #print("Dastur ishga tushdi")
 
-#for i in range(0, 10, 2):
+# for i in range(0, 10, 2):
 #    print(i)
 
 #print("Dastur tugadi")
@@ -47,3 +47,74 @@
 #    son += 1
 #    if son == 5:
 #        break
+
+
+# login = "admin"
+# passwd = "admin"
+
+
+
+# while True:
+#     input_login = input("Loginni kiriting:")
+#     passwd_login = input("Parolni kiriting:")
+#     if not input_login == login or not passwd_login == passwd:
+#         print("Login yoki parol xato")
+#         print("\n")
+#         print("=" * 30)
+#     else:
+#         print("Tizimga kirdingiz")
+#         break
+
+
+# for i in range(1, 10):
+#     print(i)
+
+
+# n = 1
+
+# while n < 11:
+#     print(n)
+#     # n = n + 1
+#     n += 1
+
+# n = 11
+
+# while n > 0:
+#     n -= 1
+    
+#     if n == 5:
+#         continue
+    
+#     print(n)
+    
+
+
+# n = 11
+
+# while n > 0:
+#     n -= 1
+    
+#     if n == 5:
+#         break
+    
+#     print(n)
+
+
+
+summ = 0
+
+while True:
+    n = int(input("Son kiriting:>"))
+
+    if n == 0:
+        print("Yigindi: ", summ)
+        break
+
+    summ += n
+
+
+# Topshiriqlar
+
+# 1) 1 dan 100 gacha juft sonlarni chiqarish(while yordamida)
+
+# 2) foydalanuvchidan 0 kiritmaguncha sonlarni qabul qilib, ularni eng kattasini topamiz
